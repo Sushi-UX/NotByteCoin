887bb42f5e970c4ee1b6ba1dcfcb27ee4a1ac322
Sushi-UX <46356596+Sushi-UX@users.noreply.github.com>
2020-05-23 18:29:48 -0400

    Initial Commit

---
+++ NotByteCoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotByteCoin
+//
+//  Created by Raymond Choy on 5/23/20.
+//  Copyright © 2020 thechoygroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
