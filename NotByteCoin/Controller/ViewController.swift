053e1826135221d584d48c604e6db47d96873949
Sushi-UX <46356596+Sushi-UX@users.noreply.github.com>
2020-05-24 15:46:48 -0400

    Modified the step numbers
    Modified the step numbers to align with my step by step instructions on the Clima app

--- NotByteCoin/Controller/ViewController.swift
+++ NotByteCoin/Controller/ViewController.swift
@@ -7,15 +7,15 @@
 //
 
 import UIKit
-                                //STEP #A2. add a protocol called UIPickerViewDataSource to the class below.
-                                //STEP #A3. After adding this new protocol an error altert will pop up. Select "fix" on the pop-up bubble.
-                                //STEP #A8. Add the protocol UIPickerViewDelegate to the class declaration
+                                //STEP #2. add a protocol called UIPickerViewDataSource to the class below.
+                                //STEP #3. After adding this new protocol an error altert will pop up. Select "fix" on the pop-up bubble.
+                                //STEP #8. Add the protocol UIPickerViewDelegate to the class declaration
 class ViewController: UIViewController {
     
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
-                                //STEP #A6. Next, we need to tell Xcode how many rows this picker should have using the pickerView:numberOfRowsInComponent: method.
+                                //STEP #6. Next, we need to tell Xcode how many rows this picker should have using the pickerView:numberOfRowsInComponent: method.
     
     var coinManager = CoinManager()
     
@@ -23,9 +23,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         coinManager.delegate = self
-                                //STEP #A4. set the ViewController.swift as the datasource for the picker. Find viewDidLoad() and add set the ViewController class as the datasource to the currencyPicker object.
+                                //STEP #4. set the ViewController.swift as the datasource for the picker. Find viewDidLoad() and add set the ViewController class as the datasource to the currencyPicker object.
         currencyPicker.dataSource = self
-                                //STEP #A9. Set the ViewController class as the delegate of the currencyPicker.
+                                //STEP #9. Set the ViewController class as the delegate of the currencyPicker.
         currencyPicker.delegate = self
     }
 }
@@ -50,9 +50,9 @@ extension ViewController: CoinManagerDelegate {
 //MARK: - UIPickerViewDataSource, UIPickerViewDelegate
 
 extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
-                                //STEP #A1. Create the following 3 IBOutlets and name them according to the image below: "bitcoinLabel", "currencyLabel" and "currencyPicker"
+                                //STEP #1. Create the following 3 IBOutlets and name them according to the image below: "bitcoinLabel", "currencyLabel" and "currencyPicker"
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-                                //STEP #A5. Now let’s actually provide the data and add the implementation for the first method numberOfComponents(in:) to determine how many columns we want in our picker. Type in "return 1"
+                                //STEP #5. Now let’s actually provide the data and add the implementation for the first method numberOfComponents(in:) to determine how many columns we want in our picker. Type in "return 1"
         return 1
     }
     
@@ -60,20 +60,20 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
         return coinManager.currencyArray.count
     }
     
-                                //STEP #A10. Next, add the delegate method pickerView(titleForRow:), start typing pickerView and select the method from the drop down list.
+                                //STEP #10. Next, add the delegate method pickerView(titleForRow:), start typing pickerView and select the method from the drop down list.
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-                                //STEP #A11. This method expects a String as an output. The String is the title for a given row. When the PickerView is loading up, it will ask its delegate for a row title and call the above method once for every row. So when it is trying to get the title for the first row, it will pass in a row value of 0 and a component (column) value of 0.
-                                //STEP #A12. So inside the method, we can use the row Int to pick the title from our currencyArray.
+                                //STEP #11. This method expects a String as an output. The String is the title for a given row. When the PickerView is loading up, it will ask its delegate for a row title and call the above method once for every row. So when it is trying to get the title for the first row, it will pass in a row value of 0 and a component (column) value of 0.
+                                //STEP #12. So inside the method, we can use the row Int to pick the title from our currencyArray.
         return coinManager.currencyArray[row]
     }
-                                //STEP #A13. Great. We’ve got a functioning Picker View. But nothing happens when you make a selection. Let’s change that. Use Xcode’s auto-suggest to add this delegate method:
+                                //STEP #13. Great. We’ve got a functioning Picker View. But nothing happens when you make a selection. Let’s change that. Use Xcode’s auto-suggest to add this delegate method:
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-                                //STEP #A14. Let’s print that row number to see which one gets selected when we scroll our UIPickerView.
+                                //STEP #14. Let’s print that row number to see which one gets selected when we scroll our UIPickerView.
 //        print(row)
-                                //STEP #A15. Let’s change that print statement to print the currency selected instead. See if you can change the code to print the currency value selected instead of the row number.
-                                //STEP #A16.Go to the CoinManager for the next step...
+                                //STEP #15. Let’s change that print statement to print the currency selected instead. See if you can change the code to print the currency value selected instead of the row number.
+                                //STEP #16.Go to the CoinManager for the next step...
 //        print(coinManager.currencyArray[row])
-                                //STEP #A17. Update the pickerView(didSelectRow:) method to pass the selected currency to the CoinManager via the getCoinPrice() method.
+                                //STEP #17. Update the pickerView(didSelectRow:) method to pass the selected currency to the CoinManager via the getCoinPrice() method.
         let selectedCurrency = coinManager.currencyArray[row]
         coinManager.getCoinPrice(for: selectedCurrency)
     }
--- NotByteCoin/Model/CoinData.swift
+++ NotByteCoin/Model/CoinData.swift
@@ -7,15 +7,15 @@
 //
 
 import Foundation
-                //STEP #C11. Create a struc and name it CoinData. This is where the data from the JSON will come back in.
-                //STEP #C12. Copy and paste the JSON url string into your web browser using Google JSON Awesome. Identify the property you want to first call and its type.
-                //STEP #C13. Declare the property in the struct below along with its type.
-                //STEP #C14. Then remember to make our struct inherit from its super class "Decodable".
-                //STEP #C15.Go back to your WeatherManager for the next step...
+                //STEP #B11. Create a struc and name it CoinData. This is where the data from the JSON will come back in.
+                //STEP #B12. Copy and paste the JSON url string into your web browser using Google JSON Awesome. Identify the property you want to first call and its type.
+                //STEP #B13. Declare the property in the struct below along with its type.
+                //STEP #B14. Then remember to make our struct inherit from its super class "Decodable".
+                //STEP #B15.Go back to your WeatherManager for the next step...
                 //>>>DECODABLE_AND_ENCODABLE_CAN_BE_REPRESENTED_BY_A_TYPE_ALIAS_CODABLE<<< STEP #E1. Change all 3 methods from "Decodable" to instead inherit from "Codable".
-                //STEP #E2.Go back to your WeatherManager for the next step...
+                //STEP #D2.Go back to your WeatherManager for the next step...
 struct CoinData: Codable {
-                //STEP #C20.Go to the URL string and using JSON Awesome. Identify the next property you want to call and its type.
-                //STEP #C21. Looking at the URL string using JSON Awesome shows that "rate" is not an array of values so we don't need to create another struct below.
+                //STEP #B20.Go to the URL string and using JSON Awesome. Identify the next property you want to call and its type.
+                //STEP #B21. Looking at the URL string using JSON Awesome shows that "rate" is not an array of values so we don't need to create another struct below.
         let rate: Double
 }
